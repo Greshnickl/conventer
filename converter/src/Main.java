b20b0d4513fc34fb84260407d8dd2551d3dc48d6
Greshnick <grisha-kushnir@mail.ru>
2023-12-10 21:17:47 +0200

    fixes

--- converter/src/Main.java
+++ converter/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Options.main_option(""); //past your token here
+        Options.main_option("3361b98bf48aabb320d342b7"); //past your token here
     }
 }
